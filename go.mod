86895519da423e816d11a71a4934d38babab1591
Ahmad Habibi <ahmadhabibi7159@gmail.com>
2023-09-24 17:15:38 +0800

    initialize

---
+++ go.mod
@@ -0,0 +1,8 @@
+module chat-app
+
+go 1.20
+
+require (
+	github.com/gorilla/websocket v1.4.1 // indirect
+	github.com/novalagung/gubrak/v2 v2.0.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,115 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/gorilla/websocket"
+	gubrak "github.com/novalagung/gubrak/v2"
+)
+
+type (
+	M             map[string]interface{}
+	SocketPayload struct {
+		Message string
+	}
+	SocketResponse struct {
+		From    string
+		Type    string
+		Message string
+	}
+	WebSocketConnection struct {
+		*websocket.Conn
+		Username string
+	}
+)
+
+const (
+	MESSAGE_NEW_USER = "New User"
+	MESSAGE_CHAT     = "Chat"
+	MESSAGE_LEAVE    = "Leave"
+)
+
+var connections = make([]*WebSocketConnection, 0)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		content, err := ioutil.ReadFile("index.html")
+		if err != nil {
+			http.Error(w, "Could not open requested file", http.StatusInternalServerError)
+			return
+		}
+
+		fmt.Fprintf(w, "%s", content)
+	})
+
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		currentGorillaConn, err := websocket.Upgrade(w, r, w.Header(), 1024, 1024)
+		if err != nil {
+			http.Error(w, "Could not open websocket connection", http.StatusBadRequest)
+		}
+
+		username := r.URL.Query().Get("username")
+		currentConn := WebSocketConnection{
+			Conn:     currentGorillaConn,
+			Username: username,
+		}
+		connections = append(connections, &currentConn)
+
+		go handleIO(&currentConn, connections)
+	})
+
+	fmt.Println("Server ready at port :8080")
+	http.ListenAndServe(":8080", nil)
+}
+
+func handleIO(currentConn *WebSocketConnection, connections []*WebSocketConnection) {
+	defer func() {
+		if r := recover(); r != nil {
+			log.Println("ERROR ", fmt.Sprintf("%v", r))
+		}
+	}()
+
+	broadCastMessage(currentConn, MESSAGE_NEW_USER, "")
+
+	for {
+		payload := SocketPayload{}
+		err := currentConn.ReadJSON(&payload)
+		if err != nil {
+			if strings.Contains(err.Error(), "websocket: close") {
+				broadCastMessage(currentConn, MESSAGE_LEAVE, "")
+				ejectConnection(currentConn)
+				return
+			}
+
+			log.Println("ERROR", err.Error())
+			continue
+		}
+
+		broadCastMessage(currentConn, MESSAGE_CHAT, payload.Message)
+	}
+}
+
+func ejectConnection(currentConn *WebSocketConnection) {
+	filtered := gubrak.From(connections).Reject(func(each *WebSocketConnection) bool {
+		return each == currentConn
+	}).Result()
+	connections = filtered.([]*WebSocketConnection)
+}
+
+func broadCastMessage(currentConn *WebSocketConnection, kind, message string) {
+	for _, eachConn := range connections {
+		if eachConn == currentConn {
+			continue
+		}
+
+		eachConn.WriteJSON(SocketResponse{
+			From:    currentConn.Username,
+			Type:    kind,
+			Message: message,
+		})
+	}
+}
