9d9ab4af3b1a8078a824173f8baf5ae7184462c8
Ahmad Habibi <ahmadhabibi7159@gmail.com>
2023-11-20 23:48:15 +0800

    on-progress: update profile handler

--- handlers/UpdateProfile.go
+++ handlers/UpdateProfile.go
@@ -1,1 +1,29 @@
 package handlers
+
+import "github.com/gofiber/fiber/v2"
+
+type (
+	updateProfileIn struct {
+		Username string `json:"username"`
+		FullName string `json:"full_name"`
+		Email    string `json:"email" validate:"email"`
+		Location string `json:"location"`
+		Website  string `json:"website"`
+	}
+	updateProfileOut struct {
+		Ok       bool   `json:"ok"`
+		Username string `json:"username"`
+		Message  string `json:"message"`
+	}
+	updateProfileError struct {
+		Ok       bool   `json:"ok"`
+		ErrorMsg string `json:"error"`
+	}
+)
+
+func UpdateProfile(c *fiber.Ctx) error {
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"ok":      true,
+		"message": "profile updated!",
+	})
+}
