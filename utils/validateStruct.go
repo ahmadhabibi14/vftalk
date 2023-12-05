1fc03a4bb2e6ff93a9f22da0163960d1692e02e8
Ahmad Habibi <ahmadhabibi7159@gmail.com>
2023-12-05 22:24:43 +0800

    fix error message for validation

--- utils/validateStruct.go
+++ utils/validateStruct.go
@@ -8,6 +8,11 @@ import (
 	"github.com/go-playground/validator/v10"
 )
 
+const (
+	ErrInvalidPassword = `Please use a stronger password with at least 8 characters, including numbers and symbols.`
+	ErrInvalidEmail    = `Invalid email address. Please check and try again.`
+)
+
 func ValidateStruct(myStruct interface{}) error {
 	validate := validator.New()
 	err := validate.Struct(myStruct)
@@ -15,9 +20,15 @@ func ValidateStruct(myStruct interface{}) error {
 		validationErrors := err.(validator.ValidationErrors)
 		errorMessages := make([]string, len(validationErrors))
 		for i, err := range validationErrors {
-			errorMessages[i] = fmt.Sprintf("Error %s validation", err.Field())
+			if strings.Contains(err.Field(), `assword`) {
+				errorMessages[i] = ErrInvalidPassword
+			} else if strings.Contains(err.Field(), `mail`) {
+				errorMessages[i] = ErrInvalidEmail
+			} else {
+				errorMessages[i] = fmt.Sprintf("Error when validation %s", err.Field())
+			}
 		}
-		msg := strings.Join(errorMessages, ", ")
+		msg := errorMessages[0]
 		return errors.New(msg)
 	}
 	return nil
---
+++ views/static/js/auth/main.js
@@ -0,0 +1,14 @@
+let eye = document.getElementById("eye");
+let eyeSlash = document.getElementById("eye-slash");
+
+function togglePassword() {
+  if (password.type === "password") {
+    password.type = "text";
+    eyeSlash.classList.replace("hidden", "block");
+    eye.classList.replace("block", "hidden");
+  } else {
+    password.type = "password";
+    eyeSlash.classList.replace("block", "hidden");
+    eye.classList.replace("hidden", "block");
+  }
+}
