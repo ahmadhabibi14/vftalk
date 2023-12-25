31598eca2920c64f9b6abac922caafec672ecd2a
Ahmad Habibi <ahmadhabibi7159@gmail.com>
2023-12-25 20:32:35 +0800

    udhfgwh43085hsdofuhnguiosdhfgiolh390htg80seyhqglufdga08ds

---
+++ handlers/apis/zCommon.go
@@ -0,0 +1,19 @@
+package apis
+
+import (
+	"vftalk/models/mailer"
+
+	"github.com/rs/zerolog"
+)
+
+type Handler struct {
+	Mailer mailer.Mailer
+	Log    zerolog.Logger
+}
+
+type HTTPResponse struct {
+	Code   int         `json:"code"`
+	Status string      `json:"status"`
+	Errors string      `json:"errors"`
+	Data   interface{} `json:"data"`
+}
