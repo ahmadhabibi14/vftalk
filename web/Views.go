5afffb6ee0f27541415dd868fbd58584cb34e84e
Ahmad Habibi <ahmadhabibi7159@gmail.com>
2024-01-20 00:01:44 +0800

    add some pages

--- web/Views.go
+++ web/Views.go
@@ -14,34 +14,26 @@ func WebViews(app *fiber.App, page *page.PageHandler) {
 	app.Get("/oauth/google", middlewares.IsLoggedIn, page.OAuthGoogle)
 	app.Get("/explore", middlewares.AuthJWT, page.Explore)
 	app.Get("/profile", middlewares.AuthJWT, page.Profile)
+	app.Get("/direct", middlewares.AuthJWT, page.DirectChat)
 
-	// 	app.Get("/direct", middlewares.AuthJWT, func(c *fiber.Ctx) error {
-	// 		userData, err := handlers.GetUserDataByUsername(c)
-	// 		if err != nil {
-	// 			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-	// 				"error": err,
-	// 			})
-	// 		}
-	// 		c.Set(fiber.HeaderContentType, "text/html; charset=utf-8")
-	// 		return c.Render("direct", fiber.Map{
-	// 			"Title":    "VFtalk | Direct Messages",
-	// 			"Username": userData.Username,
-	// 			"UserData": userData,
-	// 			"JoinAt":   utils.FormatTime(userData.JoinAt),
-	// 		}, "layouts/main")
-	// 	})
+	app.Get("/term-of-service", func(c *fiber.Ctx) error {
+		c.Set(fiber.HeaderContentType, "text/html; charset=utf-8")
+		return c.Render("termOfService", fiber.Map{
+			"Title": "VFtalk | Term of Service",
+		})
+	})
 
-	// 	app.Get("/term-of-service", func(c *fiber.Ctx) error {
-	// 		c.Set(fiber.HeaderContentType, "text/html; charset=utf-8")
-	// 		return c.Render("termOfService", fiber.Map{
-	// 			"Title": "VFtalk | Term of Service",
-	// 		})
-	// 	})
+	app.Get("/privacy-policy", func(c *fiber.Ctx) error {
+		c.Set(fiber.HeaderContentType, "text/html; charset=utf-8")
+		return c.Render("privacyPolicy", fiber.Map{
+			"Title": "VFtalk | Privacy Policy",
+		})
+	})
 
-	// 	app.Get("/privacy-policy", func(c *fiber.Ctx) error {
-	// 		c.Set(fiber.HeaderContentType, "text/html; charset=utf-8")
-	// 		return c.Render("privacyPolicy", fiber.Map{
-	// 			"Title": "VFtalk | Privacy Policy",
-	// 		})
-	// 	})
+	app.Get("/apidocs", func(c *fiber.Ctx) error {
+		c.Set(fiber.HeaderContentType, "text/html; charset=utf-8")
+		return c.Render("apidocs", fiber.Map{
+			"Title": "VFtalk | API Documentation",
+		})
+	})
 }
