aa5efa75e878891103ea8d291b5e611f0e7297ba
Ahmad Habibi <ahmadhabibi7159@gmail.com>
2024-02-07 20:50:42 +0800

    fix rate limiter response

--- middlewares/limiter.go
+++ middlewares/limiter.go
@@ -11,26 +11,28 @@ import (
 )
 
 var Limiter = limiter.Config{
-	Max:        300,
+	Max:        2,
 	Expiration: 2 * time.Minute,
 	KeyGenerator: func(c *fiber.Ctx) string {
 		return c.IP()
 	},
 	LimitReached: func(c *fiber.Ctx) error {
 		httpMethod := string(c.Request().Header.Method())
+		var message string = "You have exceeded your rate limit. Please try again a few minutes later."
+
 		if httpMethod == fiber.MethodGet {
 			c.Status(fiber.StatusTooManyRequests)
 			return c.Render("404", fiber.Map{
 				"Title":   fmt.Sprintf("%d - %s", fiber.StatusTooManyRequests, http.StatusText(fiber.StatusTooManyRequests)),
 				"Code":    fiber.StatusTooManyRequests,
 				"Status":  http.StatusText(fiber.StatusTooManyRequests),
-				"Message": "You have exceeded your rate limit. Please try again a few minutes later.",
+				"Message": message,
 			})
 		}
 		response := apis.HTTPResponse{
 			Code:   fiber.StatusTooManyRequests,
 			Status: http.StatusText(fiber.StatusTooManyRequests),
-			Errors: "You have exceeded your rate limit. Please try again a few minutes later.",
+			Errors: message,
 			Data:   "",
 		}
 		return c.Status(fiber.StatusTooManyRequests).JSON(response)
